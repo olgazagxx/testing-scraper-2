a8e74f54c9cf43cf3b03ee18cb07615d525de3a7
olgazagxx <olga.zagrubska@apify.com>
2025-11-07 21:24:00 +0100

    Add Apify Actor main function with input handling

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
+import { Actor } from 'apify';
+
+await Actor.main(async () => {
+    const input = await Actor.getInput();
+    console.log('Input received:', input);
+    await Actor.pushData({ message: 'Hello from GitHub actor!' });
+});
 
